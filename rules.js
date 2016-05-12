aa5cc1db27ed8cda8af158515d245c78b48f85c3
Spencer Elliott <spencer@eventmobi.com>
2016-05-12 14:28:45 -0400

    Use correct `rules` key in rules.js
    Previously, rules.js had no effect

--- rules.js
+++ rules.js
@@ -2,16 +2,18 @@
  * Rules shared between the base and legacy configs
  */
 module.exports = {
-  // Allow anonymous functions
-  // http://eslint.org/docs/rules/func-names
-  'func-names': 0,
-  // Allow dangling underscore, e.g. for manual Jest mocks
-  // http://eslint.org/docs/rules/no-underscore-dangle
-  'no-underscore-dangle': 0,
-  // Allow calling functions before their definition
-  // http://eslint.org/docs/rules/no-use-before-define
-  'no-use-before-define': [2, 'nofunc'],
-  // Validate JSDoc
-  // http://eslint.org/docs/rules/valid-jsdoc
-  'valid-jsdoc': [2, { requireReturn: false }],
+  rules: {
+    // Allow anonymous functions
+    // http://eslint.org/docs/rules/func-names
+    'func-names': 0,
+    // Allow dangling underscore, e.g. for manual Jest mocks
+    // http://eslint.org/docs/rules/no-underscore-dangle
+    'no-underscore-dangle': 0,
+    // Allow calling functions before their definition
+    // http://eslint.org/docs/rules/no-use-before-define
+    'no-use-before-define': [2, 'nofunc'],
+    // Validate JSDoc
+    // http://eslint.org/docs/rules/valid-jsdoc
+    'valid-jsdoc': [2, { requireReturn: false }],
+  },
 };
