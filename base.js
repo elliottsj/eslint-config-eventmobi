f23b15c6e36eb3b7a3da88f9d3ba2f7be0d75d3d
Spencer Elliott <spencer@eventmobi.com>
2016-08-02 13:09:52 -0400

    Use import/no-extraneous-dependencies
    Allow importing from
    devDependencies/optionalDependencies/peerDependencies

--- base.js
+++ base.js
@@ -7,5 +7,12 @@ module.exports = {
     // Use the spread operator instead of .apply()
     // http://eslint.org/docs/rules/prefer-spread
     'prefer-spread': 2,
+    // Forbid the use of extraneous packages
+    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-extraneous-dependencies.md
+    'import/no-extraneous-dependencies': [2, {
+      devDependencies: true,
+      optionalDependencies: true,
+      peerDependencies: true
+    }],
   },
 };
