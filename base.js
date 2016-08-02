5acc416e8ad3a104e388079e4344bd4e34f13aab
Spencer Elliott <spencer@eventmobi.com>
2016-08-02 14:23:19 -0400

    Add missing trailing comma

--- base.js
+++ base.js
@@ -19,6 +19,6 @@ module.exports = {
     'import/no-named-as-default': 0,
     // Allow modules with a single export to use a named export
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/prefer-default-export.md
-    'import/prefer-default-export': 0
+    'import/prefer-default-export': 0,
   },
 };
